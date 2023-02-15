0dcbb4705bb5b6cab7c80e6df9a6c1c21c8cdc4b
KarlaInostroza02 <77360210+KarlaInostroza02@users.noreply.github.com>
2023-02-15 15:07:41 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'library_test',
+    version = '0.0.0',
+    description = 'libreria testeo ejemplo',
+    packages = ['paquete_test'],
+    zip_safe=False)
